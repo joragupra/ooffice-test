828e9de272696f885513a0cc2701180fc6bfb0bb
Jorge Agudo Praena <Jorge.Agudo.Praena@everis.com>
2011-04-19 17:04:13 +0200

    Modificacion del proyecto para que se limite a arrancar soffice y despues de un tiempo de esara lo pare

--- src/main/java/com/ooffice/ClienteOOffice.java
+++ src/main/java/com/ooffice/ClienteOOffice.java
@@ -15,34 +15,20 @@ public class ClienteOOffice {
 	/*                          CONSTANTES PUBLICAS                       */
 	/* deberian estar configuradas en un fichero de propiedades o similar */
 	/*                                                                    */
-	public static final String OOFFICE_PATH = "/opt/openoffice.org2.4/program/soffice";
+//	public static final String OOFFICE_PATH = "/opt/openoffice.org2.4/program/soffice";
+	
+	public static final String OOFFICE_PATH = "C:\\Archivos de programa\\OpenOffice.org 2.4\\program\\soffice.exe";
 	
 	private static final String SOCKET_OPTS = "-accept=socket,host=localhost,port=";
 	
-	public static final int TIEMPO_ESPERA = 3000;   //tiempo que hay que esperar dese que se arranca el proceso soffice.bin hasta que se puede empezar a usar
+	public static final int TIEMPO_ESPERA = 30000;   //tiempo que hay que esperar dese que se arranca el proceso soffice.bin hasta que se puede empezar a usar
 	
 	public static final int PUERTO_INICIAL = 2003;  //a partir de este puerto se empiezan a buscar puertos libres para arrancar el proceso ooffice.bin
 	
-	public static final String SO_HOST = "LINUX"; //en el codigo se esperan los valores 'WINDOWS' o 'LINUX'
+	public static final String SO_HOST = "WINDOWS"; //en el codigo se esperan los valores 'WINDOWS' o 'LINUX'
 	/*                                                                    */
 	/*                     FIN  CONSTANTES PUBLICAS                       */
 	/*                                                                    */
-	
-	private static final int NUMERO_DOCUMENTOS_GENERADOS = 10;
-	
-	private static final String PIE = "Y esto va en el pie del nuevo documento.";
-
-	private static final String CABECERA = "Esto va en la cabecera";
-
-	private static final String RUTA_FICHERO_PLANTILLA = "/tmp/ooffice/ND_NOTIFICACION.sxw";
-
-	private static final String RUTA_FICHERO_GENERADO = "/tmp/ooffice/GEN_ND_NOTIFICACION.sxw";
-
-	private static final String INICIO_BOOKMARK = "inicio_decreto";
-
-	private static final String FIN_BOOKMARK = "fin_decreto";
-
-	private static final String TARGET_BOOKMARK = "inicio_decreto";
 	
 	/**
 	 * <p>
@@ -56,38 +42,11 @@ public class ClienteOOffice {
 	 */
 	public static void main(String[] args) {
 		ClienteOOffice prueba = new ClienteOOffice();
-		for (int i = 0; i < NUMERO_DOCUMENTOS_GENERADOS; i++) {
-			System.out.println("Generando documento " + (i+1));
-			prueba.generarDocumento();
-		}
-	}
-	
-	/**
-	 * <p>
-	 * Muestra c&oacute;mo se deben generar los documentos en 3 pasos para que 
-	 * no haya problemas con la memoria consumida por el proceso soffice.bin:
-	 * </p>
-	 * <p><ol>
-	 * <li>Buscar un puerto a partir del inicial que est&eacute; libre para 
-	 * poder arrancar el proceso soffice.bin</li>
-	 * <li>Iniciar el proceso soffice.bin en el puerto indicado</li>
-	 * <li>Conectar la aplicaci&oacute;n con el servidor OpenOffice arrancadoo 
-	 * en el puerto que se encontr&oacute;</li>
-	 * <li>Generar el documento</li>
-	 * </ol></p>
-	 * 
-	 * @return
-	 */
-	public int generarDocumento() {
 		System.out.println("Buscando puerto libre...");
-		ServerSocket socket = buscarPuertoLibre(PUERTO_INICIAL);
+		ServerSocket socket = prueba.buscarPuertoLibre(PUERTO_INICIAL);
 		System.out.println("Puerto encontrando: " + socket.getLocalPort());
 		System.out.println("Iniciando proceso soffice...");
-		Process p = iniciarProcesoOpenOffice(socket.getLocalPort());
-		TableGenerator.initialize(socket.getLocalPort());
-		int result = TableGenerator.betweenBookmarks(RUTA_FICHERO_PLANTILLA,
-				crearFicheroDestino(), INICIO_BOOKMARK, FIN_BOOKMARK,
-				TARGET_BOOKMARK, CABECERA, PIE);
+		Process p = prueba.iniciarProcesoOpenOffice(socket.getLocalPort());
 		System.out.println("Parando proceso soffice...");
 		p.destroy();
 		System.out.println("Liberando puerto " + socket.getLocalPort() + "...");
@@ -96,7 +55,6 @@ public class ClienteOOffice {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return result;
 	}
 	
 	public ServerSocket buscarPuertoLibre(int initPort){
@@ -146,27 +104,5 @@ public class ClienteOOffice {
 			e.printStackTrace();
 			return null;
 		}
-	}
-
-	/**
-	 * <p>
-	 * Copia el fichero de <code>RUTA_FICHERO_GENERADO</code> a&ntilde;adiendo 
-	 * un sufijo al mismo (sacado de <code>System.currentTimeMillis</code>) y 
-	 * devuelve la ruta del nuevo fichero.
-	 * </p>
-	 * 
-	 * @return
-	 */
-	private String crearFicheroDestino() {
-		File original = new File(RUTA_FICHERO_GENERADO);
-		String[] ficheroExtension = RUTA_FICHERO_GENERADO.split("\\.");
-		File copia = new File(ficheroExtension[0] + "_"
-				+ System.currentTimeMillis() + "." + ficheroExtension[1]);
-		try {
-			FileUtils.copyFile(original, copia);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return copia.getAbsolutePath();
 	}
 }
